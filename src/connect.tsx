7bc06618e4b3d09a318375b5f37c77bd054b441c
Ali Sabzevari <alisabzevari@gmail.com>
2019-05-26 10:37:41 +0200

    Improve performance of connect

--- src/connect.tsx
+++ src/connect.tsx
@@ -1,36 +1,53 @@
 import * as React from "react";
 import { ContainerContext } from "./container";
 import { MapStoresToProps, StoreMap, Unsubscribe } from "./types";
+import { Store } from "./Store";
 
 export function connect<TStoreRelatedProps, TOtherProps>(
   mapStoresToProps: MapStoresToProps<TStoreRelatedProps>
 ) {
   return (Cmp: React.ComponentClass<TStoreRelatedProps & TOtherProps>) =>
-    class ConnectedComponent extends React.Component<TStoreRelatedProps & TOtherProps> {
-      unsubscribes: Unsubscribe[] = [];
+    class ConnectedComponent extends React.Component<
+      TStoreRelatedProps & TOtherProps
+    > {
+      subscribedStores: { [storeName: string]: Unsubscribe } = {};
 
       render() {
-        const allStores: StoreMap = this.context;
+        const allStores: StoreMap = this.withAutoSubscribers(this.context);
         return <Cmp {...mapStoresToProps(allStores)} {...this.props} />;
       }
 
-      componentDidMount() {
-        const allStores: StoreMap = this.context;
-
-        Object.keys(allStores).forEach(key => {
-          const unsubscribe = allStores[key].subscribe(() => {
-            this.forceUpdate();
-          });
-          this.unsubscribes.push(unsubscribe);
-        });
-      }
-
       componentWillUnmount() {
-        this.unsubscribes.forEach(u => u());
+        Object.values(this.subscribedStores).forEach(u => u());
       }
 
-      static displayName: string = `Connect(${ConnectedComponent.name ||
-        ConnectedComponent.displayName})`;
+      subscribeToStore(name: string, store: Store) {
+        const unsubscribe = store.subscribe(() => {
+          this.forceUpdate();
+        });
+        this.subscribedStores[name] = unsubscribe;
+      }
+
+      withAutoSubscribers(stores: StoreMap): StoreMap {
+        const watchedStores: StoreMap = {};
+
+        Object.keys(stores).forEach(key => {
+          Object.defineProperty(watchedStores, key, {
+            get: () => {
+              if (!this.subscribedStores[key]) {
+                this.subscribeToStore(key, stores[key]);
+                this.subscribedStores[key];
+              }
+              return stores[key];
+            }
+          });
+        });
+
+        return watchedStores;
+      }
+
+      static displayName: string = `Connect(${Cmp.name ||
+        Cmp.displayName})`;
       static contextType = ContainerContext;
     } as React.ElementType<Partial<TStoreRelatedProps> & TOtherProps>;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,7 @@
 import { Store } from "./Store";
-import { ComponentClass } from "react";
 
 export type Listener = () => void;
 export type Unsubscribe = () => void;
 
 export type StoreMap = { [storeName: string]: Store };
 export type MapStoresToProps<T> = (stores: StoreMap) => T;
-
-
-export interface Connect {
-    <TFromStoreProps, TOwnProps>(mapStoresToProps: MapStoresToProps<TFromStoreProps>): ComponentEnhancer<TFromStoreProps, TOwnProps>;
-}
-
-export interface ComponentEnhancer<TInjectedProps, TNeedsProps> {
-    (component: ComponentClass<TNeedsProps>): ComponentClass<TNeedsProps & TInjectedProps>
-}
